a7056277490fd99ae3a53c2d24d38211e224f8d3
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-11-11 20:59:23 +0330

    loading, converting and showing images

--- Basics.py
+++ Basics.py
@@ -2,3 +2,14 @@
 import cv2
 import numpy as np
 import face_recognition
+
+# loading images and converting to RGB
+imgElon = face_recognition.load_image_file("imagesBasic/Elon-Musk-normal.jpg")
+imgElon = cv2.cvtColor(imgElon, cv2.COLOR_BGR2RGB)
+imgElonTest = face_recognition.load_image_file("imagesBasic/Elon-Musk-Test.jpg")
+imgElonTest = cv2.cvtColor(imgElonTest, cv2.COLOR_BGR2RGB)
+
+# showing images
+cv2.imshow("Elon Musk", imgElon)
+cv2.imshow("Elon Musk Test", imgElonTest)
+cv2.waitKey(0)
