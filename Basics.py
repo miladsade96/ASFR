9b074e9eebb47076d6f13861205538ee5f6dde33
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-11-12 22:28:10 +0330

    Comparing faces and calculating the distance

--- Basics.py
+++ Basics.py
@@ -20,6 +20,13 @@ encodeElonTest = face_recognition.face_encodings(imgElonTest)[0]
 cv2.rectangle(imgElonTest, (faceLocTest[3], faceLocTest[0]), (faceLocTest[1], faceLocTest[2]),
               (255, 0, 255), 2)
 
+# comparing faces
+results = face_recognition.compare_faces([encodeElon], encodeElonTest)
+# calculating distance
+faceDis = face_recognition.face_distance([encodeElon], encodeElonTest)
+cv2.putText(imgElonTest, f"{results}, {round(faceDis[0], 2)}", (50, 50),
+            cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
+
 # showing images
 cv2.imshow("Elon Musk", imgElon)
 cv2.imshow("Elon Musk Test", imgElonTest)
