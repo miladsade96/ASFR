304ac64b717a9a03d0b926f32f66df49efef796a
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-11-14 16:59:18 +0330

    implementation of images loading process

--- attendance_project.py
+++ attendance_project.py
@@ -3,3 +3,14 @@ import numpy as np
 import cv2
 import face_recognition as fr
 import os
+
+# loading images that has located in imageAttendance directory
+path = "imagesAttendance"
+images = []
+classNames = []
+myList = os.listdir(path)
+for cl in myList:
+    # current image
+    curImg = cv2.imread(f"{path}/{cl}")
+    images.append(curImg)
+    classNames.append(os.path.splitext(cl)[0])
