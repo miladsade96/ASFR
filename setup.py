7beecf82be4f71c04c3a328507ff9a458fdaff2a
ELNS <EverLookNeverSee@ProtonMail.ch>
2021-01-08 21:53:15 +0330

    configuring setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+# importing relevant packages
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="frp",
+    version="0.0.1",
+    author="Milad Sadeghi DM",
+    author_email="EverLookNeverSee@ProtonMail.ch",
+    description="facial recognition project",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/EverLookNeverSee/FRP",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operation System :: OS Independent",
+    ],
+    python_requires=">=3.7"
+)
