6725686f4de9db7ec2a39d456584b8a4af023d66
ELNS <EverLookNeverSee@ProtonMail.ch>
2021-01-11 18:21:22 +0330

    optimizing import statements

--- frp/main.py
+++ frp/main.py
@@ -10,6 +10,7 @@ import numpy as np
 from khayyam import JalaliDate
 
 from .loader import image_loader
+from .encoder import find_encodings
 
 # defining global variables
 today = ""
@@ -17,22 +18,6 @@ user_input = int
 images_list = []
 known_faces_encodes = []
 cl_names = []
-
-
-# Encoding process
-def find_encodings(list_of_images: list) -> list:
-    """
-    find encodings for each image located in the given list
-    :param list_of_images: list, all images
-    :return: list of encode values
-    """
-    encodings_list = [] # images encodings will store here
-    for img in list_of_images:
-        # convert BGR to RGB
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        encode = fr.face_encodings(img)[0]
-        encodings_list.append(encode)
-    return encodings_list
 
 
 def save_encodings(encodings: List, class_names: List):
