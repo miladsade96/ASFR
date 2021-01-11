28f8c1307e80636fd0d7cc5f52700cdb101f8eb1
ELNS <EverLookNeverSee@ProtonMail.ch>
2021-01-11 18:15:09 +0330

    optimizing import statements

--- frp/main.py
+++ frp/main.py
@@ -1,13 +1,15 @@
 # import statements
-import numpy as np
-import cv2
-import face_recognition as fr
 import os
 import pickle
 from datetime import datetime
-from typing import Tuple, List
+from typing import List
+
+import cv2
+import face_recognition as fr
+import numpy as np
 from khayyam import JalaliDate
 
+from .loader import image_loader
 
 # defining global variables
 today = ""
@@ -15,24 +17,6 @@ user_input = int
 images_list = []
 known_faces_encodes = []
 cl_names = []
-
-
-def image_loader(path: str) -> Tuple[List[str], list]:
-    """
-    Loading images from given directory
-    :param path: str, address of images directory
-    :return: list, images class names
-    """
-    images = []
-    class_names = []
-    # Adding list of contents to a list
-    list_of_contents = os.listdir(path)
-    for cl in list_of_contents:
-        current_image = cv2.imread(f"{path}/{cl}")
-        images.append(current_image)
-        # dropping the file extension
-        class_names.append(os.path.splitext(cl)[0])
-    return class_names, images
 
 
 # Encoding process
