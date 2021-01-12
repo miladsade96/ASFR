878d5d15985daff969973d46c682dd9fcb07369d
ELNS <EverLookNeverSee@ProtonMail.ch>
2021-01-12 19:13:16 +0330

    Moving encoder func to encoder.py

--- frp/main.py
+++ frp/main.py
@@ -17,22 +17,6 @@ known_faces_encodes = []
 cl_names = []
 
 
-# Encoding process
-def find_encodings(list_of_images: list) -> list:
-    """
-    find encodings for each image located in the given list
-    :param list_of_images: list, all images
-    :return: list of encode values
-    """
-    encodings_list = [] # images encodings will store here
-    for img in list_of_images:
-        # convert BGR to RGB
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        encode = fr.face_encodings(img)[0]
-        encodings_list.append(encode)
-    return encodings_list
-
-
 def save_encodings(encodings: List, class_names: List):
     """
     save encodings into a file
