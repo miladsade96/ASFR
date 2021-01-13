8c7e7dc307ec2726c5ca8afbc71b07efc555251d
ELNS <EverLookNeverSee@ProtonMail.ch>
2021-01-13 17:44:56 +0330

    Fixing path to csv files directory and some linting issues

--- frp/csv.py
+++ frp/csv.py
@@ -1,3 +1,7 @@
+"""
+    Creating csv files in order to store attendances statistics
+"""
+
 # import statement
 import os
 from khayyam import JalaliDate
@@ -8,6 +12,6 @@ def csv_creator():
     Creates a csv file for today attendances
     :return: None
     """
-    if not os.path.exists(f"./statistics/{str(JalaliDate.today())}.csv"):
-        with open(f"./statistics/{str(JalaliDate.today())}.csv", "w") as f:
-            f.writelines(f"Name, Time")
+    if not os.path.exists(f"../statistics/{str(JalaliDate.today())}.csv"):
+        with open(f"../statistics/{str(JalaliDate.today())}.csv", "w") as file:
+            file.writelines("Name, Time")
