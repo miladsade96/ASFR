8729ffa0d5e2d8ed4a8bbc6d88f5b51b211eaebc
ELNS <EverLookNeverSee@ProtonMail.ch>
2021-01-16 21:28:26 +0330

    Refactoring function to get an image instead of list of images as parameter and return its encoding value

--- frp/encoder.py
+++ frp/encoder.py
@@ -1,21 +1,17 @@
 """Generating encode values(vectors) for each image located in imagesAttendance directory."""
 
 # import statements
-from typing import List
 import cv2
 import face_recognition as fr
 
 
-def encoder(list_of_images: List) -> List:
+def encoder(image):
     """
     Find encodings for each image located in the given list.
-    :param list_of_images: list, all images
-    :return: list of encode values
+    :param image: loaded image
+    :return: encode value
     """
-    encodings_list = []     # images encodings will store here
-    for img in list_of_images:
-        # convert BGR to RGB
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        encode = fr.face_encodings(img)[0]
-        encodings_list.append(encode)
-    return encodings_list
+    # convert BGR to RGB
+    img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    encode = fr.face_encodings(img)[0]
+    return encode
