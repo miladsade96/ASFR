9e7fdfe024dbac0a2b80166f20b5a7bc642a608d
ELNS <EverLookNeverSee@ProtonMail.ch>
2021-01-30 22:46:05 +0330

    declaring two new list to store previous records skipping first row of csv file skipping new line character passing declared time format instead of str

--- asfr/marker.py
+++ asfr/marker.py
@@ -17,12 +17,16 @@ def attendance_marker(name_of_person: str) -> None:
     fmt = "%H:%M:%S"    # time format
     with open(f"../statistics/{str(JalaliDate.today())}.csv", "r+") as file:
         my_data_list = file.readlines()
-        list_of_names = []
-        for line in my_data_list:
-            entry = line.split(", ")
-            list_of_names.append(entry[0])
-        if name_of_person not in list_of_names:
+        # declaring two empty lists to store previous records
+        names = []
+        times = []
+        for line in my_data_list[1:]:   # skipping first row(Name,Time)
+            line = line.strip()     # skipping new line character
+            entry = line.split(",")
+            names.append(entry[0])
+            times.append(entry[1])
+        if name_of_person not in names:
             now = datetime.now()
-            date_string = now.strftime("%H:%M:%S")
-            file.writelines(f"\n{name_of_person}, {date_string}")
+            date_string = now.strftime(fmt)
+            file.writelines(f"\n{name_of_person},{date_string}")
             file.flush()
