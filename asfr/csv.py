f85aa2bd26162dcd3be91b657e113b78c35db08e
ELNS <EverLookNeverSee@ProtonMail.ch>
2021-02-06 12:33:16 +0330

    creating statistics directory at cold start

--- asfr/csv.py
+++ asfr/csv.py
@@ -2,6 +2,7 @@
 
 # import statement
 import os
+from pathlib import Path
 from khayyam import JalaliDate
 
 
@@ -10,6 +11,7 @@ def csv_creator():
     Creates a csv file for today attendances.
     :return: None
     """
+    Path("../statistics").mkdir(parents=True, exist_ok=True)
     if not os.path.exists(f"../statistics/{str(JalaliDate.today())}.csv"):
         with open(f"../statistics/{str(JalaliDate.today())}.csv", "w") as file:
             file.writelines("Name,Time")
