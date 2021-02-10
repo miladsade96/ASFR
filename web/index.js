643cd64fb4c9a2e8c9495cab607de849de6dbe51
Mohammadreza <moharnadreza@gmail.com>
2021-02-10 14:40:07 +0330

    feat: init webcam access

--- web/index.js
+++ web/index.js
@@ -1,1 +1,15 @@
-//
+var video = document.querySelector("#videoElement");
+
+if (navigator.mediaDevices.getUserMedia) {
+  navigator.mediaDevices
+    .getUserMedia({ video: true })
+    .then(function (stream) {
+      video.srcObject = stream;
+      video.reload();
+      video.play();
+    })
+    .catch(function (error) {
+      console.log("Something went wrong!");
+      console.error(error);
+    });
+}
