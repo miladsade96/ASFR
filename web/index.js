94517e6dec07aa9d255e069c23fc5b1727218b2e
Mohammadreza <moharnadreza@gmail.com>
2021-02-10 16:10:21 +0330

    feat: init webcam access

--- web/index.js
+++ web/index.js
@@ -1,5 +1,20 @@
-var video = document.querySelector("#videoElement");
+const date = document.querySelector("#date");
+const progress = document.querySelector("#encodeProgress");
+const video = document.querySelector("#videoElement");
 
+// change date value
+const dateOptions = {
+  weekday: "long",
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+};
+date.textContent = new Date().toLocaleDateString("en-US", dateOptions);
+
+// change progress bar value
+progress.value = 80;
+
+// webcam access grant
 if (navigator.mediaDevices.getUserMedia) {
   navigator.mediaDevices
     .getUserMedia({ video: true })
