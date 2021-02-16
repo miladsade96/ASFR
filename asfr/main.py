4d0bd0d3bafd1dcd430ffb036779e1eacffe3a9d
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-02-16 13:09:41 +0330

    fixing image_loader function

--- asfr/main.py
+++ asfr/main.py
@@ -23,22 +23,21 @@ cl_names = []
 
 
 @eel.expose
-def image_loader(path="../train_images") -> Tuple[List[str], list]:
+def image_loader(path="../train_images") -> None:
     """
     Loading images from given directory.
     :param path: str, address of images directory
-    :return: list, images class names
+                default value: train_images directory
+    :return: None
     """
-    images = []
-    class_names = []
+    global images_list, cl_names
     # Adding list of contents to a list
     list_of_contents = os.listdir(path)
     for cl in list_of_contents:
         current_image = cv2.imread(f"{path}/{cl}")
-        images.append(current_image)
+        images_list.append(current_image)
         # dropping the file extension
-        class_names.append(os.path.splitext(cl)[0])
-    return class_names, images
+        cl_names.append(os.path.splitext(cl)[0])
 
 
 def _encoder(image):
