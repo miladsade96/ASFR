501b614f1789bd78b29d350a3c4b4009ca1d6029
Mohammadreza <moharnadreza@gmail.com>
2021-02-26 19:46:57 +0330

    fix: eel actions

--- web/index.js
+++ web/index.js
@@ -1,6 +1,6 @@
 const date = document.querySelector("#date");
 const progress = document.querySelector("#encodeProgress");
-const video = document.querySelector("#videoElement");
+const image = document.querySelector("#cameraRenderer");
 const loadButton = document.querySelector("#load");
 const encodeButton = document.querySelector("#encode");
 const saveButton = document.querySelector("#save");
@@ -23,9 +23,9 @@ function updateProgress(value) {
 }
 
 // will load svg file
-function loadCSVFile() {
+function loadCSVFile(filePath) {
   // change csv file address here
-  d3.text("addresses.csv", function (data) {
+  d3.text(filePath, function (data) {
     const parsedCSV = d3.csv.parseRows(data);
 
     d3.select(".statistics")
@@ -46,62 +46,47 @@ function loadCSVFile() {
   });
 }
 
+function loadImage(file) {
+  // Check if the file is an image.
+  if (file.type && file.type.indexOf("image") === -1) {
+    return;
+  }
+
+  const reader = new FileReader();
+  reader.addEventListener("load", (event) => {
+    image.src = event.target.result;
+  });
+  reader.readAsDataURL(file);
+}
+
 // will fire if `load` button's click action triggered
 loadButton.addEventListener("click", function () {
   // runs eel's image_loader function
-  console.log("`load` button clicked!"); // remove this line
   eel.image_loader();
 });
 
 // will fire if `encode` button's click action triggered
 encodeButton.addEventListener("click", function () {
   // runs eel's encoder function
-  console.log("`encode` button clicked!"); // remove this line
   eel.encoder();
 });
 
 // will fire if `save` button's click action triggered
 saveButton.addEventListener("click", function () {
   // runs eel's save_encodings function
-  console.log("`save` button clicked!"); // remove this line
   eel.save();
 });
 
 // will fire if `start` button's click action triggered to display csv file as a table
 startButton.addEventListener("click", function () {
-  console.log("`start` button clicked!"); // remove this line
-  if (statistics.innerHTML === "") {
-    // runs eel's recognizer function
-    eel.recognizer().then(function () {
-      // webcam access grant
-      if (navigator.mediaDevices.getUserMedia) {
-        navigator.mediaDevices
-          .getUserMedia({ video: true })
-          .then(function (stream) {
-            video.srcObject = stream;
-            video.load();
-            video.play();
-          })
-          .catch(function (error) {
-            console.log("Something went wrong!");
-            console.error(error);
-          });
-      }
-
-      // runs eel's csv_reader function
-      eel.csv_reader().then(function () {
-        loadCSVFile();
-      });
-    });
-  }
+  // runs eel's recognizer function
+  eel.recognizer().then(function (imageFile) {
+    loadImage(imageFile);
+  });
 });
 
 // will fire if `stop` button's click action triggered to remove video src
 stopButton.addEventListener("click", function () {
-  console.log("`stop` button clicked!"); // remove this line
-  const mediaStream = video.srcObject;
-  const tracks = mediaStream.getTracks();
-  tracks.forEach((track) => track.stop());
-  video.src = "";
-  eel.stop();
+  image.src = "./images/blank.png";
+  // eel.stop();
 });
