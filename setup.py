06fbdca5861b686589444e8bc5e85d91a9359a7f
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-03-03 21:02:10 +0330

    fixing setup.py  issues and changing version number

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="frp",
-    version="0.0.1",
+    name="ASFR",
+    version="1.3.3",
     author="Milad Sadeghi DM",
     author_email="EverLookNeverSee@ProtonMail.ch",
-    description="facial recognition project",
+    description="Attendance system using facial recognition",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/EverLookNeverSee/FRP",
+    url="https://github.com/EverLookNeverSee/ASFR",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
